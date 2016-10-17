f6a8a8d51c0e5cdbc16395aa12084bf6c84cff07
VinayGupta23 <vinay.23.gupta@outlook.com>
2016-10-18 00:49:57 +0530

    Bug fix - Add random initialization code

--- main.m
+++ main.m
@@ -34,6 +34,16 @@ ytest(ytest == 0) = 10;
 %  advanced optimizers are able to train our cost functions efficiently as
 %  long as we provide them with the gradient computations.
 
+% Compute random initial weights for the network
+fprintf('\nInitializing Neural Network Parameters ...\n')
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+% Unroll parameters
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+% Train network
 fprintf('\nTraining Neural Network... \n')
 
 %  After you have completed the assignment, change the MaxIter to a larger
